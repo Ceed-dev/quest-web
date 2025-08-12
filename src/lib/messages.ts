d232337302d83d2e0cf7b86dc4800f04fcf9ef23
shungo0222 <kimura.shungo@gmail.com>
2025-08-12 12:06:04 +0900

    Implement language switch logic and UI button

---
+++ src/lib/messages.ts
@@ -0,0 +1,24 @@
+export const messages = {
+  en: {
+    header: {
+      nav: {
+        about: "About us",
+        caseStudies: "Case Studies",
+        services: "Services",
+        questPlatform: "Quest Platform",
+      },
+      cta: { apply: "Apply to Work With Us" },
+    },
+  },
+  ja: {
+    header: {
+      nav: {
+        about: "会社概要",
+        caseStudies: "事例紹介",
+        services: "サービス",
+        questPlatform: "Questプラットフォーム",
+      },
+      cta: { apply: "提携申請応募" },
+    },
+  },
+};
